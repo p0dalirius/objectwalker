27a651d43c267013223292905b7abbd33e2c9b53
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-26 10:22:35 +0200

    Created objectwalker/

---
+++ objectwalker/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : __init__.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 26 Apr 2023
+
+from .core import *
+from objectwalker.filters.filter_object import *
