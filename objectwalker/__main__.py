fa3280077cc98a952b28fe86feeff3d555121895
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-26 10:55:39 +0200

    Added more options to standalone objectwalker console_script

--- objectwalker/__main__.py
+++ objectwalker/__main__.py
@@ -6,6 +6,7 @@
 
 import argparse
 import objectwalker
+from objectwalker.filters import *
 
 VERSION = "1.1"
 
@@ -16,9 +17,16 @@ def parseArgs():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-v", "--verbose", default=False, action="store_true", help='Verbose mode. (default: False)')
     parser.add_argument("-d", "--max-depth", default=5, type=int, help="Maximum recursion depth to explore in objects.")
+    parser.add_argument("--no-colors", dest="no_colors", action="store_true", default=False, help="No colors mode.")
 
-    source = parser.add_mutually_exclusive_group(required=True)
-    source.add_argument("-m", "--module", default=None, type=str, help="Python module to explore")
+    group_source = parser.add_mutually_exclusive_group(required=True)
+    group_source.add_argument("-m", "--module", default=None, type=str, help="Python module to explore.")
+
+    group_filters = parser.add_argument_group("Filters")
+    group_filters.add_argument("--filter-object-name", default=None, type=str, help="Show paths from the base object leading object named <name>.")
+    group_filters.add_argument("--filter-object-is-module", default=False, action="store_true", help="Show paths from the base object leading to modules.")
+    group_filters.add_argument("--filter-object-is-builtin-function-or-method", default=False, action="store_true", help="Show paths from the base object leading to builtin-function-or-method.")
+    group_filters.add_argument("--filter-object-is-method-wrapper", default=False, action="store_true", help="Show paths from the base object leading to method-wrapper.")
 
     return parser.parse_args()
 
@@ -26,11 +34,19 @@ def parseArgs():
 def main():
     options = parseArgs()
 
-    print("[>] Starting to explore ...")
+    filters = []
+    if options.filter_object_is_module:
+        filters.append(FilterObjIsModule())
+    if options.filter_object_is_module:
+        filters.append(FilterObjIsModule())
+    if options.filter_object_is_module:
+        filters.append(FilterObjIsModule())
+
+    print("[>] Exploring object tree of module '%s' up to depth %d ..." % (options.module, options.max_depth))
     module = __import__(options.module)
-    ow = objectwalker.core.ObjectWalker(filters=[FilterObjIsBuiltinFunctionOrMethod()], verbose=False)
+    ow = objectwalker.core.ObjectWalker(filters=filters, verbose=options.verbose, no_colors=options.no_colors)
     ow.walk(module, path=[options.module])
-    print("[>] all done!")
+    print("[>] All done!")
 
 
 if __name__ == '__main__':
