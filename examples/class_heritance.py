8b5efb697dcc984387897e2676d2ebc09afc992d
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-26 10:54:50 +0200

    Added examples

---
+++ examples/class_heritance.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : class_heritance.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 26 Apr 2023
+
+import objectwalker
+from objectwalker.filters import *
+
+SECRET = "UEHPAcW3TSaZAl6rVW32wwRTgl9lsVF"
+
+class A(object):
+    def __init__(self):
+        super(A, self).__init__()
+
+class B(A):
+    def __init__(self):
+        super(B, self).__init__()
+
+
+B_obj = B()
+
+print("[>] Starting to explore ...")
+ow = objectwalker.core.ObjectWalker(filters=[FilterObjIsBuiltinFunctionOrMethod()], verbose=False)
+ow.walk(B_obj, path=["B_obj"])
+print("[>] all done!")
