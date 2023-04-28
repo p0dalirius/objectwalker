a2aff9738ef65c4ae76b8ad1f6d8eaffbb619a59
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-28 14:10:05 +0200

    Updated setup.py and Release 2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,42 @@
 
 import setuptools
 
-long_description = """e"""
+long_description = """
+<p align="center">
+    A python module to explore the object tree to extract paths to interesting objects in memory.
+    <br>
+    <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/p0dalirius/objectwalker">
+    <a href="https://twitter.com/intent/follow?screen_name=podalirius_" title="Follow"><img src="https://img.shields.io/twitter/follow/podalirius_?label=Podalirius&style=social"></a>
+    <a href="https://www.youtube.com/c/Podalirius_?sub_confirmation=1" title="Subscribe"><img alt="YouTube Channel Subscribers" src="https://img.shields.io/youtube/channel/subscribers/UCF_x5O7CSfr82AfNVTKOv_A?style=social"></a>
+    <br>
+</p>
+
+## Features
+
+ - [x] Python module to use in pdb after a `breakpoint()`.
+ - [x] Standalone tool to explore paths in python modules.
+ - [x] Multiple built-in filters.
+ - [x] Possibility to implement custom filters and pass them to ObjectWalker().
+
+## Installation
+
+You can now install it from pypi with this command:
+
+```
+sudo python3 -m pip install objectwalker
+```
+
+## Example commands
+
+ + We want to find all the paths to the `os` module in the module `jinja2`:
+    ```
+    objectwalker -m jinja2 --filter-object-is-module os --max-depth 15
+    ```
+
+## Contributing
+
+Pull requests are welcome. Feel free to open an issue if you want to add other features.
+"""
 
 with open('requirements.txt', 'r', encoding='utf-8') as f:
     requirements = [x.strip() for x in f.readlines()]
