35bd90bd48bb1abed53abe564eec499f695302ce
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-29 20:04:07 +0200

    Added matching of regular expressions in FilterObjectNameStartsWith.py

--- objectwalker/filters/object/name/FilterObjectNameStartsWith.py
+++ objectwalker/filters/object/name/FilterObjectNameStartsWith.py
@@ -4,7 +4,8 @@
 # Author             : Podalirius (@podalirius_)
 # Date created       : 26 Apr 2023
 
-from .EmptyFilter import EmptyFilter
+from objectwalker.filters.EmptyFilter import EmptyFilter
+from objectwalker.utils import RegExMatcher
 
 
 class FilterObjectNameStartsWith(EmptyFilter):
@@ -12,20 +13,25 @@ class FilterObjectNameStartsWith(EmptyFilter):
     Documentation for class FilterObjectNameStartsWith
     """
     values = []
+    regular_expressions = []
     no_colors = False
 
     filter_name = "FilterObjectNameStartsWith"
 
-    def __init__(self, values, no_colors=False):
+    def __init__(self, values, regular_expressions=[], no_colors=False):
         super(FilterObjectNameStartsWith, self).__init__()
         self.callback = self.print_result
         self.no_colors = no_colors
         self.values = values
+        self.regular_expressions = regular_expressions
 
     def check(self, obj, path_to_obj):
         matches_filter = False
 
-        if any([(str(path_to_obj[-1]).startswith(value)) for value in self.values]):
+        regexmatcher = RegExMatcher(regular_expressions=self.regular_expressions)
+        regexmatcher.set_all_regex_to_startswith()
+
+        if any([(str(path_to_obj[-1]).startswith(value)) for value in self.values]) or (regexmatcher.match(path_to_obj[-1])):
             matches_filter = True
 
         if matches_filter:
@@ -35,4 +41,4 @@ class FilterObjectNameStartsWith(EmptyFilter):
         return matches_filter
 
     def __repr__(self):
-        return "<%s values=%s>" % (self.filter_name, self.values)
+        return "<%s values=%s regular_expressions=%s>" % (self.filter_name, self.values, self.regular_expressions)
