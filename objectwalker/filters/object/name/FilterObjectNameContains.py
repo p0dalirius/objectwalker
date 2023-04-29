4960f0a072cae20d1e42b1e43e047eb5d39c5396
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-29 20:03:31 +0200

    Added matching of regular expressions in FilterObjectNameContains.py

--- objectwalker/filters/object/name/FilterObjectNameContains.py
+++ objectwalker/filters/object/name/FilterObjectNameContains.py
@@ -4,7 +4,8 @@
 # Author             : Podalirius (@podalirius_)
 # Date created       : 26 Apr 2023
 
-from .EmptyFilter import EmptyFilter
+from objectwalker.filters.EmptyFilter import EmptyFilter
+from objectwalker.utils import RegExMatcher
 
 
 class FilterObjectNameContains(EmptyFilter):
@@ -14,20 +15,25 @@ class FilterObjectNameContains(EmptyFilter):
     Filters
     """
     values = []
+    regular_expressions = []
     no_colors = False
 
     filter_name = "FilterObjectNameContains"
 
-    def __init__(self, values, no_colors=False):
+    def __init__(self, values, regular_expressions=[], no_colors=False):
         super(FilterObjectNameContains, self).__init__()
         self.callback = self.print_result
         self.no_colors = no_colors
         self.values = values
+        self.regular_expressions = regular_expressions
 
     def check(self, obj, path_to_obj):
         matches_filter = False
 
-        if any([(value in str(path_to_obj[-1])) for value in self.values]):
+        regexmatcher = RegExMatcher(regular_expressions=self.regular_expressions)
+        regexmatcher.set_all_regex_to_contains()
+
+        if any([(value in str(path_to_obj[-1])) for value in self.values]) or (regexmatcher.match(path_to_obj[-1])):
             matches_filter = True
 
         if matches_filter:
@@ -37,4 +43,4 @@ class FilterObjectNameContains(EmptyFilter):
         return matches_filter
 
     def __repr__(self):
-        return "<%s values=%s>" % (self.filter_name, self.values)
+        return "<%s values=%s regular_expressions=%s>" % (self.filter_name, self.values, self.regular_expressions)
