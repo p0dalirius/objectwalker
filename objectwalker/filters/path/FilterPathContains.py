312fb6644eba4ac375cfd2c8659a8c486357ca2c
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-29 20:05:07 +0200

    Added matching of regular expressions in FilterPathContains.py

--- objectwalker/filters/path/FilterPathContains.py
+++ objectwalker/filters/path/FilterPathContains.py
@@ -4,7 +4,7 @@
 # Author             : Podalirius (@podalirius_)
 # Date created       : 26 Apr 2023
 
-from .EmptyFilter import EmptyFilter
+from objectwalker.filters.EmptyFilter import EmptyFilter
 
 
 class FilterPathContains(EmptyFilter):
@@ -12,6 +12,7 @@ class FilterPathContains(EmptyFilter):
     Documentation for class FilterPathContains
     """
     values = []
+    regular_expressions = []
     no_colors = False
 
     filter_name = "FilterPathContains"
@@ -25,7 +26,10 @@ class FilterPathContains(EmptyFilter):
     def check(self, obj, path_to_obj):
         matches_filter = False
 
-        if any([value in '.'.join(path_to_obj) for value in self.values]):
+        regexmatcher = RegExMatcher(regular_expressions=self.regular_expressions)
+        regexmatcher.set_all_regex_to_contains()
+
+        if any([value in '.'.join(path_to_obj) for value in self.values]) or (regexmatcher.match(path_to_obj[-1])):
             matches_filter = True
 
         if matches_filter:
@@ -35,4 +39,4 @@ class FilterPathContains(EmptyFilter):
         return matches_filter
 
     def __repr__(self):
-        return "<%s values=%s>" % (self.filter_name, self.values)
+        return "<%s values=%s regular_expressions=%s>" % (self.filter_name, self.values, self.regular_expressions)
