98f102bca5826d7618163f1c470605b95052b273
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-29 20:03:40 +0200

    Added matching of regular expressions in FilterObjectNameEndsWith.py

--- objectwalker/filters/object/name/FilterObjectNameEndsWith.py
+++ objectwalker/filters/object/name/FilterObjectNameEndsWith.py
@@ -4,7 +4,8 @@
 # Author             : Podalirius (@podalirius_)
 # Date created       : 26 Apr 2023
 
-from .EmptyFilter import EmptyFilter
+from objectwalker.filters.EmptyFilter import EmptyFilter
+from objectwalker.utils import RegExMatcher
 
 
 class FilterObjectNameEndsWith(EmptyFilter):
@@ -16,16 +17,20 @@ class FilterObjectNameEndsWith(EmptyFilter):
 
     filter_name = "FilterObjectNameEndsWith"
 
-    def __init__(self, values, no_colors=False):
+    def __init__(self, values, regular_expressions=[], no_colors=False):
         super(FilterObjectNameEndsWith, self).__init__()
         self.callback = self.print_result
         self.no_colors = no_colors
         self.values = values
+        self.regular_expressions = regular_expressions
 
     def check(self, obj, path_to_obj):
         matches_filter = False
 
-        if any([(str(path_to_obj[-1]).endswith(value)) for value in self.values]):
+        regexmatcher = RegExMatcher(regular_expressions=self.regular_expressions)
+        regexmatcher.set_all_regex_to_endswith()
+
+        if any([(str(path_to_obj[-1]).endswith(value)) for value in self.values]) or (regexmatcher.match(path_to_obj[-1])):
             matches_filter = True
 
         if matches_filter:
@@ -35,4 +40,4 @@ class FilterObjectNameEndsWith(EmptyFilter):
         return matches_filter
 
     def __repr__(self):
-        return "<%s values=%s>" % (self.filter_name, self.values)
+        return "<%s values=%s regular_expressions=%s>" % (self.filter_name, self.values, self.regular_expressions)
