9b874a384d1ade58c80462800b56c8e231afda0e
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-04-29 20:02:52 +0200

    Added Reporter.py

---
+++ objectwalker/utils/Reporter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : Reporter.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 29 Apr 2023
+
+class Reporter(object):
+    """
+    Documentation for class Reporter
+    """
+
+    def __init__(self):
+        super(Reporter, self).__init__()
