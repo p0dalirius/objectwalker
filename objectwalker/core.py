p0dalirius/objectwalker

commit 5ce4e4dddad86860da62912392009320ffa84e60

Fixed inverted filter_matchmode

objectwalker/core.py
@@ -154,8 +154,8 @@ class ObjectWalker(object):
                             continue
 
                         # Save the found path if it matches filters (accept, and not reject)
-                        if not self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_reject]):
-                            if self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_accept]):
+                        if not self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_accept]):
+                            if self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_reject]):
                                 found.append(path_to_obj)
                         else:
                             # Save id of explored object
@@ -218,8 +218,8 @@ class ObjectWalker(object):
                             path_to_obj.append(subkey)
 
                         # Save the found path if it matches filters (accept, and not reject)
-                        if not self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_reject]):
-                            if self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_accept]):
+                        if not self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_accept]):
+                            if self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_reject]):
                                 found.append(path_to_obj)
                         else:
                             # Save id of explored object

commit a8492b4099712e47071f8351c287a5cb8f70cf72

Fixed implementation error introduced in commit 5ce4e4dddad86860da62912392009320ffa84e60 in filters matchmode

objectwalker/core.py
@@ -193,8 +193,8 @@ class ObjectWalker(object):
                             path_to_obj.append(subkey)
 
                         # Save the found path if it matches filters (accept, and not reject)
-                        if not self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_accept]):
-                            if self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_reject]):
+                        if not self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_reject]):
+                            if self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_accept]):
                                 found.append(path_to_obj)
                         else:
                             # Save id of explored object
@@ -253,8 +253,8 @@ class ObjectWalker(object):
                             path_to_obj.append(subkey)
 
                         # Save the found path if it matches filters (accept, and not reject)
-                        if not self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_accept]):
-                            if self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_reject]):
+                        if not self.filter_matchmode_reject([f.check(subobj, path_to_obj) for f in self.filters_reject]):
+                            if self.filter_matchmode_accept([f.check(subobj, path_to_obj) for f in self.filters_accept]):
                                 found.append(path_to_obj)
                         else:
                             # Save id of explored object